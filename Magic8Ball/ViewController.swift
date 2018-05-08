b5a637b9b50e02dd4133eafba3e797df25f9c1cf
juan hurtado <juanhurtado@MacBook-Pro-de-juan.local>
2018-05-07 23:40:17 -0500

    primer commit

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivBall: UIImageView!
+    var randomNumberValue:Int=0
+    let arrayAnswer:[String]=["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        changeImageViewBackground(myImage: ivBall)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func actionBallAsking(_ sender: UIButton) {
+        
+        changeImageViewBackground(myImage: ivBall)
+        
+    }
+    
+    func changeImageViewBackground(myImage:UIImageView) -> Void {
+        randomNumberValue=Int(arc4random_uniform(5))
+        myImage.image=UIImage.init(named: arrayAnswer[randomNumberValue])
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion==UIEventSubtype.motionShake {
+            changeImageViewBackground(myImage: ivBall)
+        }
+    }
 }
 
